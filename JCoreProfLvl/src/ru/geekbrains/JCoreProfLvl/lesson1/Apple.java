5d88d9a4ae94a96b651202003ca39ea1c3940f40
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-09-24 19:59:59 +0300

    Класс Apple

--- JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson1/Apple.java
+++ JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson1/Apple.java
@@ -1,4 +1,11 @@
 package ru.geekbrains.JCoreProfLvl.lesson1;
 
-public class Apple {
+public class Apple extends Fruit {
+    public Apple(){
+        super(1.0);
+    }
+
+    public Apple(double weight) {
+        super(weight);
+    }
 }
