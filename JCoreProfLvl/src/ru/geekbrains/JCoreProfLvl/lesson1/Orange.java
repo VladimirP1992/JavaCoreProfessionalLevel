a63905584ce9b8d229253f960835b8a49ddec7ab
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-09-24 20:00:12 +0300

    Класс Orange

--- JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson1/Orange.java
+++ JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson1/Orange.java
@@ -1,4 +1,11 @@
 package ru.geekbrains.JCoreProfLvl.lesson1;
 
-public class Orange {
+public class Orange extends Fruit {
+    public Orange(){
+        super(1.5);
+    }
+
+    public Orange(double weight) {
+        super(weight);
+    }
 }
