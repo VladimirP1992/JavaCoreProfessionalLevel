a90059d7b0a310131828a55da20450d10e05bd79
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-10-05 19:55:53 +0300

    методы для увеличения шага, его обнуления и т.д.

--- JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson4/TurnCounter.java
+++ JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson4/TurnCounter.java
@@ -1,4 +1,27 @@
 package ru.geekbrains.JCoreProfLvl.lesson4;
 
 public class TurnCounter {
+    public final int MAX_TURN;
+    private int turn = 0;
+
+    public TurnCounter(int maxTurn){
+        MAX_TURN = maxTurn;
+    }
+
+    public int getCurrentTurn() {
+        return turn;
+    }
+
+    public boolean needsReset(){
+        return turn > MAX_TURN;
+    }
+
+    public void reset(){
+        turn = 0;
+        System.out.println();
+    }
+
+    public void nextTurn() {
+        turn++;
+    }
 }
