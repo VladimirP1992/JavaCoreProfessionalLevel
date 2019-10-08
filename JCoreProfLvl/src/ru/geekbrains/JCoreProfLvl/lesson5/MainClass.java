987b6d966ab906de17f8028066494b53bd1335ef
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-10-08 22:39:07 +0300

    Семафор с половинной пропускной способностью передается в конструктор Tunnel; гонка начинается когда все Car.isReadyForRace == true; гонка заканчивается когда CountDownLatch rivalsAtFinish опустится до нуля в методах Car.run()

--- JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson5/MainClass.java
+++ JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson5/MainClass.java
@@ -1,4 +1,41 @@
 package ru.geekbrains.JCoreProfLvl.lesson5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Semaphore tunnelSemaphore = new Semaphore(CARS_COUNT/2);
+
+        Race race = new Race(new Road(60), new Tunnel(tunnelSemaphore), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        CyclicBarrier rivalsAtStart = new CyclicBarrier(CARS_COUNT);
+        CountDownLatch rivalsAtFinish = new CountDownLatch(CARS_COUNT);
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), rivalsAtStart, rivalsAtFinish);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        boolean allCarsReady;
+        do{
+            allCarsReady = true;
+            for (Car car : cars){
+                allCarsReady &= car.isReadyForRace();
+            }
+        }while (!allCarsReady);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        try {
+            rivalsAtFinish.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
 }
