dce48c9a6127c6ac575fdf60962adbc1afd1069b
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-10-22 22:05:04 +0300

    В главном классе запускается тестировщик

--- JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson7/MainClass.java
+++ JCoreProfLvl/src/ru/geekbrains/JCoreProfLvl/lesson7/MainClass.java
@@ -1,4 +1,9 @@
 package ru.geekbrains.JCoreProfLvl.lesson7;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class MainClass {
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        MyClassTester.start(ClassForTest.class);
+    }
 }
